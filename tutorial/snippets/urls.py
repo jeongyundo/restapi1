4a4b6bc69c521a19d7094f382402c3b2f29709ca
정윤도 <truenorthj@gmail.com>
2019-08-06 20:26:03 +0900

    django drf tutorial done

--- tutorial/snippets/urls.py
+++ tutorial/snippets/urls.py
@@ -1,11 +1,13 @@
-from django.conf.urls import url
-from rest_framework.urlpatterns import format_suffix_patterns
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from snippets import views
 
-urlpatterns = [
-    url(r'^snippets/$', views.snippet_list),
-    url(r'^snippets/(?P<pk>[0-9]+)$', views.snippet_detail),
-]
-#r'^ ~~ ' 를 사용해야 리스트안으로 들어가야 다른 패턴에 사용가능하다.
+# Create a router and register our viewsets with it.
+router = DefaultRouter()
+router.register(r'snippets', views.SnippetViewSet)
+router.register(r'users', views.UserViewSet)
 
-urlpatterns = format_suffix_patterns(urlpatterns)
+# The API URLs are now determined automatically by the router.
+urlpatterns = [
+    path('', include(router.urls)),
+]
